667d9c666258463dddc5f0d438997dd3244e6587
Saenz-Dev <miguel.saenz02@uptc.edu.co>
2025-11-15 22:33:39 -0500

    Guardar Completado, solo falta posicionar bien el cuadro de guardar persona, hacer responsive la página, y la tabla que tenga tamaño fijo y scroll, y falta modificar y eliminar, aunque esas son mas faciles

--- js/crudPersons.js
+++ js/crudPersons.js
@@ -7,6 +7,7 @@ $(function () {
 /**  CONSTANTES DE BOTONES DEL CRUD  **/
 
 const btnActualizar = $('.btn-actualizar');
+let contadorAlert = 0;
 
 /**  CONSTANTE DE URL PARA ACCEDER A SERVICIOS RESTful  **/
 
@@ -56,17 +57,26 @@ function row(id, name, lastName, phone) {
 }
 
 function mostrarMensaje(success, mensaje) {
-    const alert = $(".alert");
-    alert.removeClass('alert-danger alert-success');
-    if (success) alert.addClass('alert-success');
-    if (!success) alert.addClass('alert-danger');
-    alert.text(mensaje);
+    let typeAlert = success ? 'alert-success' : 'alert-danger';
+    $('.alertaGlobal').append(`<div class="alert ${typeAlert} my-3 alertDangerItems" id="alert${contadorAlert++}">${mensaje}</div>`).css("font-weight", "bold");
+    const alert = $('.alertaGlobal');
     alert.stop(true, true).fadeIn(400);
-    alert.removeClass("d-none");
     setTimeout(() => {
-        alert.fadeOut(500);
-    }, 5000);
+        $('.alertaGlobal').fadeOut(400, function () {
+            $(this).empty();
+        });
+    }, 6000);
 }
+
+// function mostrarMensaje(success, mensaje) {
+//     $('.alertaGlobal').fadeIn(400);
+//     $('.alertaGlobal').append(`<div class="alert alert-danger my-3 alertDangerItems" id="alert${contadorAlert++}">${mensaje}</div>`).css("font-weight", "bold");
+//     setTimeout(() => {
+//         $('.alertaGlobal').fadeOut(400, function () {
+//             $(this).empty();
+//         });
+//     }, 6000);
+// }
 
 /**  AGREGAR PERSONAS  **/
 
@@ -78,8 +88,41 @@ function obtenerDatosCrearPersona() {
     }
 }
 
-function validarDatosVacios() {
-    
+function validarTextoVacio(input, divValidation, mensajeValido, mensajeInvalido) {
+    if (input.val().length < 3) {
+        input.removeClass('is-valid').addClass('is-invalid');
+        divValidation.removeClass('valid-feedback').addClass('invalid-feedback').text(mensajeInvalido);
+        return false;
+    } else {
+        input.removeClass('is-invalid').addClass('is-valid');
+        divValidation.removeClass('invalid-feedback').addClass('valid-feedback').text(mensajeValido);
+        return true;
+    }
+}
+
+function validarNumeroTelefonico(input, divValidation, mensajeValido, mensajeInvalido) {
+    const regex = /^3\d{9}$/;
+    if (!regex.test(input.val())) {
+        input.removeClass('is-valid').addClass('is-invalid');
+        divValidation.removeClass('valid-feedback').addClass('invalid-feedback').text(mensajeInvalido);
+        return false;
+    } else {
+        input.removeClass('is-invalid').addClass('is-valid');
+        divValidation.removeClass('invalid-feedback').addClass('valid-feedback').text(mensajeValido);
+        return true;
+    }
+}
+
+function vaciarInputCrearPersona() {
+    $('#nombres').val('');
+    $('#apellidos').val('');
+    $('#telefono').val('');
+}
+
+function resetearCrearPersona() {
+    $('#nombres').removeClass('is-valid is-invalid');
+    $('#apellidos').removeClass('is-valid is-invalid');
+    $('#telefono').removeClass('is-valid is-invalid');
 }
 
 function postHTTP() {
@@ -98,8 +141,47 @@ function postHTTP() {
         });
 }
 
+function validarIngresoDatos() {
+    let nombreIsValid = validarTextoVacio($('#nombres'), $('#validation-name'), 'Estructura Válida', 'El nombre debe tener 3 letras o más');
+    let apellidoIsValid = validarTextoVacio($('#apellidos'), $('#validation-lastname'), 'Estructura Válida', 'El apellido debe tener 3 letras o más');
+    let telefonoIsValid = validarNumeroTelefonico($('#telefono'), $('#validation-phone'), 'Estructura Válida', 'El teléfono debe iniciar con el dígito tres y nueve digitos más');
+    if (nombreIsValid && apellidoIsValid && telefonoIsValid) $('#btn-crear-persona').removeClass('disabled');
+    if (!(nombreIsValid && apellidoIsValid && telefonoIsValid)) $('#btn-crear-persona').addClass('disabled');
+}
+
 $('#btn-crear-persona').on('click', function () {
     postHTTP();
     getHTTP();
-})
+    $('.create-person').fadeOut(500);
+    vaciarInputCrearPersona();
+    resetearCrearPersona();
+    $('#btn-crear-persona').addClass('disabled');
+    mostrarMensaje(true, "Persona Creada.");
+});
+
+$('#nombres').on('input', function () {
+    validarIngresoDatos();
+});
+
+$('#apellidos').on('input', function () {
+    validarIngresoDatos();
+});
+
+$('#telefono').on('input', function () {
+    validarIngresoDatos();
+});
+
+
+/**  EVENTO PARA ACTIVAR LA VENTANA DE CREAR PERSONAS  **/
+
+$('.btn-crear').on('click', function () {
+    $('.create-person').fadeIn(500);
+});
+
+$('#btn-cancelar-crear-persona').on('click', function () {
+    $('.create-person').fadeOut(500);
+    vaciarInputCrearPersona();
+    resetearCrearPersona();
+    mostrarMensaje(false, 'Ha cancelado la creación de Persona.');
+});
 
